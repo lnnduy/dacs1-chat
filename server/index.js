5ff63198407ffd42fb13d4f8b5f78bb3d625c31c
lnnd <lnnduy68@gmail.com>
2020-04-02 15:42:12 +0700

    use yarn to build client app

--- server/index.js
+++ server/index.js
@@ -35,8 +35,8 @@ app.use("/api/users", require("./routes/users"));
 
 app.use("/uploads", express.static("uploads"));
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("../client/build"));
+if (process.env.NODE_ENV !== "production") {
+  app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
